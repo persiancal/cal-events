c06fa1f51c073ebe8e7f812c17d741aa1e1d44e8
fzerorubigd <fzerorubigd@gmail.com>
2019-09-25 00:25:22 +0200

    add reorder tool
    add the year key to sample

---
+++ cmd/thetool/reorder.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+
+	"gopkg.in/yaml.v2"
+)
+
+var reorderOut *string
+
+func reorder(cmd *Command, fl *File) error {
+	sort.Sort(fl)
+	f := os.Stdout
+	var err error
+	if *reorderOut != "-" {
+		f, err := os.Create(*reorderOut)
+		if err != nil {
+			return fmt.Errorf("open target file failed: %w", err)
+		}
+		defer f.Close()
+	}
+
+	b, err := yaml.Marshal(fl)
+	if err != nil {
+		return fmt.Errorf("failed to marshal yaml: %w", err)
+	}
+
+	_, err = f.Write(b)
+	return err
+}
+
+func init() {
+	cmd := NewCommand("reorder", "reorder the input yaml file", reorder)
+	reorderOut = cmd.Flags.String("output", "-", "teh output, - for stdout")
+	Register(cmd)
+}
